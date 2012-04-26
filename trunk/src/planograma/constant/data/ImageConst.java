6955d6e6951307a8a127c2c2a9126fab6c129241
polyakov.alexandr.alexandrovich@gmail.com <polyakov.alexandr.alexandrovich@gmail.com@1e046652-7f85-7666-9cd7-bf192973eb37>
2012-04-26 04:34:56 +0000

    загрузка избражений, кеш картинок

---
+++ trunk/src/planograma/constant/data/ImageConst.java
@@ -0,0 +1,15 @@
+package planograma.constant.data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Admin
+ * Date: 26.04.12
+ * Time: 6:13
+ * To change this template use File | Settings | File Templates.
+ */
+public interface ImageConst {
+	public static final String TABLE_NAME = "mz.image";
+
+	public static final String CODE_IMAGE = "code_image";
+	public static final String IMAGE = "image";
+}
---
+++ trunk/src/planograma/model/ImageModel.java
@@ -0,0 +1,77 @@
+package planograma.model;
+
+import planograma.constant.data.ImageConst;
+import planograma.data.UserContext;
+import planograma.utils.FileUtils;
+
+import java.io.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Admin
+ * Date: 26.04.12
+ * Time: 5:33
+ * To change this template use File | Settings | File Templates.
+ */
+public class ImageModel {
+	private final File tempDir;
+	public static final String Q_SELECT = "select" +
+			" " + ImageConst.IMAGE + " " +
+			"from " + ImageConst.TABLE_NAME + " " +
+			"where " + ImageConst.CODE_IMAGE + " = ?";
+
+	public InputStream select(final UserContext userContext, final int code_image) throws SQLException, IOException {
+		//		long time=System.currentTimeMillis();
+		InputStream in = null;
+		File file = new File(tempDir, String.valueOf(code_image));
+		if (!file.isFile()) {
+			final Connection connection = userContext.getConnection();
+			final PreparedStatement ps = connection.prepareStatement(Q_SELECT);
+			ps.setInt(1, code_image);
+			final ResultSet resultSet = ps.executeQuery();
+			if (resultSet.next()) {
+				in = resultSet.getBinaryStream(1);
+				FileUtils.copy(in, new FileOutputStream(file));
+			}
+		}
+		try {
+			in = new FileInputStream(file);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+//		System.out.println(System.currentTimeMillis()-time);
+		return in;
+	}
+
+	public void clearCache() {
+		if (tempDir != null) {
+			for (final File f : tempDir.listFiles())
+				FileUtils.delete(f);
+		}
+	}
+
+	private static ImageModel instance = new ImageModel();
+
+	public static ImageModel getInstance() {
+		return instance;
+	}
+
+	private ImageModel() {
+		final String path = System.getProperty("java.io.tmpdir");
+		tempDir = new File(path, "planogram/image/");
+		tempDir.mkdirs();
+	}
+
+	@Override
+	protected void finalize() throws Throwable {
+		FileUtils.delete(tempDir);
+	}
+
+	public static void main(String args[]) {
+		final ImageModel instance = getInstance();
+	}
+}
